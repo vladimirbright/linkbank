cc46b1b3c793816464463d459d86f2745e1ff3da
Владимир <vladimirbright@gmail.com>
2011-02-25 22:23:18 +0300

    фаб

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from fabric.api import *
-from fabric.contrib.console import confirm
-from fabric.utils import abort
+# from fabric.contrib.console import confirm
+# from fabric.utils import abort
+
+
+env.project_dir = "/home/knbase.info/linkbank"
+env.home_dir = "/home/knbase.info/linkbank"
 
 
 @hosts('knbase.info@knbase.info')
 def ppull():
     u""" git pull on developer@192.168.0.103"""
-    with cd('~/linkbank'):
+    with cd(env.project_dir):
         run('git pull', pty=True)
 
 
 @hosts('knbase.info@knbase.info')
 def prestart():
-    with cd('~'):
-        run('kill -HUP `cat django.pid`')
+    run('kill -HUP `cat ' + env.home_dir + '/django.pid`')
 
 
 @hosts('knbase.info@knbase.info')
