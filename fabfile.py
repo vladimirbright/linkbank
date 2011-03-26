183eb27738d7ff1c0987714f35b4a30c25eeec0d
Владимир <vladimirbright@gmail.com>
2011-03-26 20:14:09 +0300

    refactor fabfile

--- fabfile.py
+++ fabfile.py
@@ -10,23 +10,38 @@ env.home_dir = "/home/knbase.info/linkbank"
 
 
 @hosts('knbase.info@knbase.info')
-def ppull():
-    u""" git pull on developer@192.168.0.103"""
+def git_pull():
     with cd(env.project_dir):
         run('git pull', pty=True)
 
 
 @hosts('knbase.info@knbase.info')
-def prestart():
-    run('kill -HUP `cat ' + env.home_dir + '/django.pid`')
+def django_hup():
+    run('kill -HUP `cat ' + env.home_dir + '/django.pid`', pty=True)
 
 
-@hosts('knbase.info@knbase.info')
-def killall():
-    run('killall python')
+@hosts('root@knbase.info')
+def django_stop():
+    run('supervisorctl stop knbase.info', pty=True)
+
+
+@hosts('root@knbase.info')
+def django_start():
+    run('supervisorctl start knbase.info', pty=True)
+
+
+@hosts('root@knbase.info')
+def django_restart():
+    run('supervisorctl restart knbase.info', pty=True)
+
+
+@hosts('root@knbase.info')
+def django_status():
+    run('supervisorctl status knbase.info', pty=True)
 
 
 @hosts('knbase.info@knbase.info')
 def update():
-    ppull()
-    killall()
+    git_pull()
+    django_hup()
+
