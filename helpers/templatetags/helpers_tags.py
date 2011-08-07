5b1367b9bd987e2ae0bfde3284e7477ce25e15c1
Владимир <vladimirbright@gmail.com>
2011-08-07 23:51:43 +0400

    qr google chart api

--- helpers/templatetags/helpers_tags.py
+++ helpers/templatetags/helpers_tags.py
@@ -5,6 +5,14 @@ from django import template
 register = template.Library()
 
 @register.filter
+def qr(value,size="150x150"):
+    """
+        Usage:
+            <img src="{{object.code|qr:"120x130"}}" />
+    """
+    return "http://chart.apis.google.com/chart?chs=%s&cht=qr&chl=%s&choe=UTF-8&chld=H|0" % (size, value)
+
+@register.filter
 def multiple(a, b):
     return a * b
 
