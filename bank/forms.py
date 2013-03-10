e3bef24609c7b8311ef5077a21b7e49e910ae229
Владимир Прохода <vladimirbright@gmail.com>
2013-03-10 16:03:40 +0400

    Добавляем определение кодировки в букмарклете и форме

--- bank/forms.py
+++ bank/forms.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from codecs import lookup
+
+
 from captcha.fields import CaptchaField
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.forms.models import modelformset_factory
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
+
 from bank.models import Link, Profile, ImportTask, ExportTask
 
 
-LinkDeleteFormSet = modelformset_factory(Link, fields=[], can_delete=True, extra=0)
+LinkDeleteFormSet = modelformset_factory(Link, fields=(), can_delete=True, extra=0)
 
 
 class ExportBookmarksForm(forms.ModelForm):
@@ -43,10 +46,27 @@ class ProfileEditForm(forms.ModelForm):
 
 
 class LinkForm(forms.ModelForm):
-    return_me_to_link = forms.BooleanField(
-                            label=_("Return me to link after save"),
-                            required=False
-                        )
+
+    DEFAULT_LINK_ENCODING = 'utf8'
+
+    return_me_to_link = forms.BooleanField(label=_("Return me to link after save"),
+                                           required=False)
+    encoding = forms.CharField(required=False,
+                               initial=DEFAULT_LINK_ENCODING,
+                               widget=forms.HiddenInput)
+
+    @classmethod
+    def check_encoding(cls, request):
+        enc = request.REQUEST.get('encoding', '').strip()
+        if enc:
+            try:
+                lookup(enc)
+            except LookupError:
+                enc = LinkForm.DEFAULT_LINK_ENCODING
+        else:
+            enc = LinkForm.DEFAULT_LINK_ENCODING
+        request.encoding = enc
+        return request
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop("user")
